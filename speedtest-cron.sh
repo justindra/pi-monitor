c984f34ae4b0fbd95f9482bc8a0b04b9af6f2530
Justin Rahardjo <justindra91@gmail.com>
2020-11-05 09:56:48 -0800

    feat: auto-commit to git

--- speedtest-cron.sh
+++ speedtest-cron.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 sudo python /home/pi/pi-monitor/speedtest.py >> /home/pi/pi-monitor/speedtest.csv
+cd /home/pi/pi-monitor
+git add .
+git commit -m "chore: updated speedtest recording"
+git push origin master
